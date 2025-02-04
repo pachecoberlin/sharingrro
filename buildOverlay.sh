178d895af279901b1dd20d0c0e536b00c64c11fb
pacheco <pacheco@gmx.de>
2025-02-04 13:35:02 +0100

    testing sysui overlay

--- buildOverlay.sh
+++ buildOverlay.sh
@@ -1,22 +1,22 @@
 rm Overlay.flata
-rm myoverlays.apk.u
-rm myoverlays.apk
+rm systemuirro.apk.u
+rm systemuirro.apk
 
 aapt2 compile -v --dir res/ -o Overlay.flata
 aapt2 link -v --no-resource-removal \
 -I ~/Android/Sdk/platforms/android-35/android.jar \
 --manifest AndroidManifest.xml \
--o myoverlays.apk.u Overlay.flata
-zipalign 4 myoverlays.apk.u myoverlays.apk
-printf 'android' | jarsigner -keystore ~/.android/debug.keystore  myoverlays.apk androiddebugkey
+-o systemuirro.apk.u Overlay.flata
+zipalign 4 systemuirro.apk.u systemuirro.apk
+printf 'android' | jarsigner -keystore ~/.android/debug.keystore  systemuirro.apk androiddebugkey
 
 adb connect 192.168.0.50
 adb root
-adb shell cmd overlay disable de.pacheco.rro
-adb shell pm uninstall de.pacheco.rro
+adb shell cmd overlay disable de.pacheco.rro.systemui
+adb shell pm uninstall de.pacheco.rro.systemui
 adb remount
 adb shell  mount -o remount,rw /
-adb push myoverlays.apk product/overlay
-adb shell cmd overlay enable de.pacheco.rro
+adb push systemuirro.apk product/overlay
+adb shell cmd overlay enable de.pacheco.rro.systemui
 adb shell cmd overlay list
 # adb reboot
